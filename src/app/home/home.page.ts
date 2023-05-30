fa5bb1ad0fd359a81470f94e6983e1fc774f9106
Marcilio Freitas <marcilio@beltsistemas.com.br>
2023-05-29 23:52:06 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonTextarea, IonicModule } from '@ionic/angular';
+import { TextToSpeech } from '@capacitor-community/text-to-speech';
+import { RangeValue } from '@ionic/core';
+import { RangeCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +12,35 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class HomePage {
+
+  taxa!: RangeValue;
+  tom!: RangeValue;
   constructor() {}
+
+  public speak = async (text: IonTextarea) => {
+    await TextToSpeech.speak({
+      text: JSON.stringify(text.value),
+      lang: 'pt-BR',
+      rate: +(this.taxa),
+      pitch: +(this.tom),
+      volume: 1.0,
+      category: 'ambient',
+    });
+  };
+
+  public stop = async () => {
+    await TextToSpeech.stop();
+  };
+
+  public taxaChange(event: Event){
+    this.taxa = (event as RangeCustomEvent).detail.value;
+  }
+
+  public tomChange(event: Event){
+    this.tom = (event as RangeCustomEvent).detail.value;
+  }
+
+  public clear = (text: IonTextarea) => {
+    text.value = "";
+  }
 }
